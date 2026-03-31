21666b44aaba8976713f2f896c4157a7c2ed3390
Malaka heshan <heshanmalaka12@gmail.com>
2026-03-31 11:51:32 +0530

    test

---
+++ CustomerCareApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.sliit.customer_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
